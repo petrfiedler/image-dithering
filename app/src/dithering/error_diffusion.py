4d9715ea5bf66091760046d5e81c45dfd19c7a23
Petr Fiedler <fiedlpe5@fit.cvut.cz>
2022-04-27 22:31:23 +0200

    semestral: Update error diffusion implementation.

--- app/src/dithering/error_diffusion.py
+++ app/src/dithering/error_diffusion.py
@@ -1,22 +1,7 @@
 import numpy as np
 from app.src.dithering.error_diffusion_maps import ERROR_DIFFUSION_MAPS
 from scipy.spatial import KDTree
-
-
-def findClosestColor(pixel: np.ndarray, palette: np.ndarray,
-                     paletteTree: KDTree) -> np.ndarray:
-    """Find closest color in color palette.
-
-    Args:
-        pixel (np.ndarray): original color
-        palette (np.ndarray): color palette
-        paletteTree (KDTree): octree with the same color palette
-
-    Returns:
-        np.ndarray: closest color from the color palette
-    """
-    index = paletteTree.query(pixel)[1]
-    return palette[index]
+from app.src.dithering.nns import findClosestColor
 
 
 def applyError(error: np.ndarray, pixel: np.ndarray,
@@ -39,11 +24,12 @@ def applyError(error: np.ndarray, pixel: np.ndarray,
 
 
 def dither(img: np.ndarray, palette: np.ndarray, map: str) -> np.ndarray:
-    """Dither image using Floyd-Steinberg algorithm.
+    """Dither image using error diffusion algorithm.
 
     Args:
         img (np.ndarray): image to dither
         palette (np.ndarray): color palette to use
+        map (str): name of error diffusion map
 
     Returns:
         np.ndarray: dithered image
